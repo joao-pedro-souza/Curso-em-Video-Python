7108e9eec60b52c3e70a496dab5cb57da5b165e6
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-19 12:07:29 -0300

    Update ex002.py

--- exercicios-curso-em-video/ex002.py
+++ exercicios-curso-em-video/ex002.py
@@ -1,3 +1,3 @@
 #Programa que pede o nome do usuário e mostra uma mensagem de boas-vindas
 nome = input('Digite seu nome: ')
-print(f'É um prazer te conhecer, \33[30;4;107m{nome}\33[m')
+print(f'É um prazer te conhecer, {nome}')
