16cde478f88b0040853222d7afd25bdeb44f3e12
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-19 13:14:47 -0300

    Update ex007.py

--- exercicios-curso-em-video/ex007.py
+++ exercicios-curso-em-video/ex007.py
@@ -1,3 +1,4 @@
+#Programa que pede duas notas e exibe a média entre elas
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 print(f'A média é {(n1+n2)/2:.1f}')
