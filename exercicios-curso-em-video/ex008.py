2ec05f4e5438bd60165814fe558c1251abbb4d4e
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-19 13:18:03 -0300

    Update ex008.py

--- exercicios-curso-em-video/ex008.py
+++ exercicios-curso-em-video/ex008.py
@@ -1,5 +1,6 @@
+#Programa que pede uma distância em metros e faz a conversão para centímetros, milímetros, decímetros, decâmetros, hectômetros e quilômetros
 m = int(input('Qual é a distância em metros? '))
-print(f'{m} metros equivalem à {m*100} centímetros, ou {m*1000} mílimetros')
+print(f'{m} metros equivalem à {m*100} centímetros, ou {m*1000} milímetros')
 print(f'DM = {m*10}')
 print(f'DAM = {m/10}')
 print(f'HM = {m/100}')
