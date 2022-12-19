c6625e5b67bb3f551a1d23294de9ec4c86014713
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-19 13:02:15 -0300

    Update ex021.py

--- exercicios-curso-em-video/ex021.py
+++ exercicios-curso-em-video/ex021.py
@@ -1,9 +1,7 @@
 #Programa que toca um áudio em mp3
-
 import pygame
 pygame.mixer.init()
 pygame.init()
-
 pygame.mixer.music.load('lifelike-126735.mp3')
 pygame.mixer.music.play(loops=0, start=0.0)
 pygame.event.wait()
