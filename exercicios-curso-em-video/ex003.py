4079e5d2266c75aa1a393ce800db8310daa35640
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-19 12:09:28 -0300

    Update ex003.py

--- exercicios-curso-em-video/ex003.py
+++ exercicios-curso-em-video/ex003.py
@@ -2,4 +2,4 @@
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
 s = n1 + n2
-print(f'A soma entre \33[32m{n1} \33[me \33[33m{n2} \33[mé \33[34m{s}')
+print(f'A soma entre {n1} e {n2} é {s}')
