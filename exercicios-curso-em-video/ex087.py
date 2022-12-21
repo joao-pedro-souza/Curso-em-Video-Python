429b5ac6054ebea0ed3ea64ec38bc4b3579191ae
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-21 11:51:17 -0300

    Update ex087.py

--- exercicios-curso-em-video/ex087.py
+++ exercicios-curso-em-video/ex087.py
@@ -1,12 +1,16 @@
 matriz = [[0, 0, 0],
                 [0, 0, 0],
                 [0, 0, 0]]
+somapar = 0
 
 for l in range(0, 3):
     for c in range(0, 3):
-        matriz[l][c] = input(f'Digite o número da posição {l}, {c}: ')
+        matriz[l][c] = int(input(f'Digite o número da posição {l}, {c}: '))
+        if matriz[l][c] % 2 == 0:
+            somapar += matriz[l][c]
 
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]}]', end='')
     print()
+print(f'A soma de números pares é {somapar}')
