ac7496b37b3a9ad19b10281b838e1993dc4af427
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-21 11:48:28 -0300

    Update ex087.py

--- exercicios-curso-em-video/ex087.py
+++ exercicios-curso-em-video/ex087.py
@@ -1,3 +1,12 @@
-matriz = [[0, 0, 0]
-                [0, 0, 0]
+matriz = [[0, 0, 0],
+                [0, 0, 0],
                 [0, 0, 0]]
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = input(f'Digite o número da posição {l}, {c}: ')
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]}]', end='')
+    print()
