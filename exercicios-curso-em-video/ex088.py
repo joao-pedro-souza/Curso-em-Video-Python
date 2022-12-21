17d48b40d3121362690dfed2bf10d25d83221221
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-21 12:27:36 -0300

    Update ex088.py

---
+++ exercicios-curso-em-video/ex088.py
@@ -0,0 +1,19 @@
+from random import randint
+sorteio = list()
+lista = list()
+cont = 0
+quant = int(input('Quantos sorteios quer fazer? '))
+for n in range(0, quant):
+    while True:
+        num = randint(0, 60)
+        if num not in sorteio:
+            sorteio.append(num)
+            cont += 1
+            if cont == 6:
+                cont = 0
+                sorteio.sort()
+                lista.append(sorteio[:])
+                sorteio.clear()
+                break
+for x in range(0, quant):
+    print(f'Jogo {x+1}: {lista[x]}')
