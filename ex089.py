3fcb03149dda5f8a700cd611703da7c4fb31caca
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-21 14:03:15 -0300

    Update ex089.py

---
+++ ex089.py
@@ -0,0 +1,17 @@
+lista = list()
+c = 0
+while True:
+    c += 1
+    nome = input('Nome: ')
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    lista.append([nome, [nota1, nota2], media])
+
+    r = input('Quer continuar? [S/N] ')
+    if r.lower() == 'n':
+        break
+
+print('Nº'.ljust(5), 'Nome'.ljust(20), 'Média')
+for i, a in enumerate(lista):
+    print(f'{i}'.ljust(5), f'{a[0]}'.ljust(20), f'{a[2]}')
