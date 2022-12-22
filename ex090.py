f1654c77257c2e41a62cddbae8425bfcbc3f7e5c
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-22 16:39:37 -0300

    Update ex090.py

--- ex090.py
+++ ex090.py
@@ -1,3 +1,4 @@
+#Programa que lê nome, média e situação de um aluno e guarda em um dicionário e mostra na tela
 dicionario = dict()
 dicionario['Nome'] = input('Nome do aluno: ')
 dicionario['Média'] = float(input('Média do aluno: '))
