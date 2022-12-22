497141ecb29f1fcd7cd8e728ebfa95fb98be7d1d
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-22 17:02:37 -0300

    Update ex091.py

---
+++ ex091.py
@@ -0,0 +1,13 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+jogo = {'Jogador 1': randint(1, 6), 'Jogador 2': randint(1, 6), 'Jogador 3': randint(1, 6), 'Jogador 4': randint(1, 6)}
+for k, v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print('Ranking:')
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar {v[0]}, com {v[1]}')
+    sleep(1)
