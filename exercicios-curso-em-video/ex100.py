d460d39c00d5838fa140c10a357114896688a487
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-23 17:09:36 -0300

    Update ex100.py

---
+++ exercicios-curso-em-video/ex100.py
@@ -0,0 +1,21 @@
+from random import randint
+numeros = list()
+
+def sorteia(lista):
+    print('Sorteando 5 valores da lista: ', end='')
+    for c in range (0, 5):
+        n = randint(0, 10)
+        lista.append(n)
+        print(n, end=' ')
+    print('PRONTO!')
+
+def somaPar(lista):
+    soma = 0
+    for c in lista:
+        if c % 2 == 0:
+            soma += c
+    print(f'Somando os valores pares de {numeros}, temos {soma}')
+
+
+sorteia(numeros)
+somaPar(numeros)
