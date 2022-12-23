70ad9a90f303d899e9201c743f4aa0d56d57803a
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-23 17:15:06 -0300

    Update ex021.py

--- exercicios-curso-em-video/ex021.py
+++ exercicios-curso-em-video/ex021.py
@@ -2,6 +2,6 @@
 import pygame
 pygame.mixer.init()
 pygame.init()
-pygame.mixer.music.load('lifelike-126735.mp3')
+pygame.mixer.music.load('audio.mp3')
 pygame.mixer.music.play(loops=0, start=0.0)
 pygame.event.wait()
