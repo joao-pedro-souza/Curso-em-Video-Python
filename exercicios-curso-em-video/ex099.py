cbda97841f197013c32d2b1a93ea0efbfe07ec1b
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-23 16:55:31 -0300

    Update ex099.py

---
+++ exercicios-curso-em-video/ex099.py
@@ -0,0 +1,15 @@
+def maior(*num):
+    maior = 0
+    print('Analisando os valores passados:')
+    for n in num:
+        print(n, end=' ')
+        if n > maior:
+            maior = n
+    print(f'foram informados {len(num)} valores ao todo.')
+    print(f'O maior valor informado foi {maior}.\n')
+
+
+maior(0, 2, 4)
+maior(7, 8, 78, 1, -4)
+maior()
+maior(1)
