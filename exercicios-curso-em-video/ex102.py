b9d673dc7d4c3741d48f4b4ec044f61123ede99c
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-24 11:57:53 -0300

    Update ex102.py

---
+++ exercicios-curso-em-video/ex102.py
@@ -0,0 +1,13 @@
+def fatorial(n, show=False):
+    f = 1
+    for c in range (n, 0, -1):
+        f *= c
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+    return f
+    
+print(fatorial(8, True))
