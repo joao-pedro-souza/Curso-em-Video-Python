a192166d976257b11c4561307c7d7534940522ce
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-24 13:01:58 -0300

    Update ex105.py

---
+++ exercicios-curso-em-video/ex105.py
@@ -0,0 +1,31 @@
+def notas(*tupla, sit=False):
+    dic = dict()
+    total = 0
+    maior = 0
+    menor = 0
+    soma = 0
+    for n in tupla:
+        total += 1
+        soma += n
+        if n > maior:
+            maior = n
+        if menor == 0:
+            menor = n
+        elif n < menor:
+            menor = n
+    dic['total'] = total
+    dic['maior'] = maior
+    dic['menor'] = menor
+    dic['media'] = soma / total
+    if sit:
+        if soma / total >= 7:
+            dic['situação'] = 'BOA'
+        elif soma / total >= 5:
+            dic['situação'] = 'RAZOÁVEL'
+        elif soma / total < 5:
+            dic['situação'] = 'RUIM'
+    return dic
+
+
+resp = notas(5, 10, 8, 7.5, sit=True)
+print(resp)
