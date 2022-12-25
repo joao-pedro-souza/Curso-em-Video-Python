e88be67dcbd9fc7a3d812319bf567e0f07b6a0bd
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-25 13:08:26 -0300

    Update ex107.py

---
+++ exercicios-curso-em-video/ex107.py
@@ -0,0 +1,7 @@
+import moeda
+
+n = float(input('Digite o preço: R$'))
+print(f'A metade de R${n} é R${moeda.metade(n)}')
+print(f'O dobro de R${n} é R${moeda.dobro(n)}')
+print(f'Aumentando 10%, temos R${moeda.aumentar(n)}')
+print(f'Diminuindo 13%, temos R${moeda.diminuir(n)}')
