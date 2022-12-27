2ad26a3f4032431b0ec78a11243036d6e198320d
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-27 11:24:40 -0300

    atualizei as funções aumentar e diminuir

--- exercicios-curso-em-video/moeda.py
+++ exercicios-curso-em-video/moeda.py
@@ -6,12 +6,12 @@ def dobro(n):
     return n * 2
 
 
-def aumentar(n):
-    return n + n * 10 / 100
+def aumentar(n, t):
+    return n + n * t / 100
 
 
-def diminuir(n):
-    return n - n * 13 / 100
+def diminuir(n, t):
+    return n - n * t / 100
 
 
 def moeda(n):
