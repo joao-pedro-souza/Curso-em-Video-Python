c6bc73097e86b704cf3759cd128bd891507709a8
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-28 20:58:02 -0300

    atualizei ex112

--- exercicios-curso-em-video/ex112/utilidadescev/dado/__init__.py
+++ exercicios-curso-em-video/ex112/utilidadescev/dado/__init__.py
@@ -1,9 +1,9 @@
 def leiaDinheiro(msg):
     valido = False
     while not valido:
-        entrada = str(input(msg)).replace(',', '.')
-        if entrada.isalpha():
-            print(f'\033[0;31mERRO: {entrada} é um preço inválido\033[m')
+        entrada = str(input(msg)).replace(',', '.').strip()
+        if entrada.isalpha() or entrada == '':
+            print(f'\033[0;31mERRO: "{entrada}" é um preço inválido\033[m')
         else:
             válido = True
             return float (entrada)
