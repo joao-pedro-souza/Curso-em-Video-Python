fa3419e441cdc2ebacfb6795980030daea3afa61
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-30 20:24:10 -0300

    comentei ex012

--- exercicios-curso-em-video/ex012.py
+++ exercicios-curso-em-video/ex012.py
@@ -1,2 +1,3 @@
+#Programa que lê o preço de um produto e mostra na tela qual é o preço com 5% de desconto
 p = int(input('Qual o preço do produto? '))
 print(f'O novo preço com 5% de desconto é {p-(p*5/100):.2f}')
