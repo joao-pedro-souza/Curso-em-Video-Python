7e90d75acdde740819bc041c672f7aef595a7324
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-30 20:22:52 -0300

    comentei ex011

--- exercicios-curso-em-video/ex011.py
+++ exercicios-curso-em-video/ex011.py
@@ -1,3 +1,4 @@
+#Programa que lê altura e largura de uma parede e diz quantos litros de tinta serão necessários para pintark
 h = float(input('Digite a altura da parede: '))
 l = float(input('Digite a largura da parede: '))
 a = h * l
