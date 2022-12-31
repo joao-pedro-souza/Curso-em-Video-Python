d023707060bc5f63625c5457e1e1db7b18c5c1c3
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2022-12-31 20:06:23 -0300

    comentei ex014

--- exercicios-curso-em-video/ex014.py
+++ exercicios-curso-em-video/ex014.py
@@ -1,2 +1,3 @@
+#Programa que lê uma temperatura em graus Celsius e converte para Fahrenheit
 c = float(input('Qual é a temperatura em graus Celsius? '))
 print(f'{c}°C correspondem à {c*9/5+32} graus Fahrenheit')
