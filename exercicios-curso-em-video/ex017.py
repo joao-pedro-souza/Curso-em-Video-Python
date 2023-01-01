40fcba840519f62423c377f38edb744ff6b7dda7
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-01-01 17:05:59 -0300

    comentei ex017

--- exercicios-curso-em-video/ex017.py
+++ exercicios-curso-em-video/ex017.py
@@ -1,4 +1,5 @@
+#Programa que lê dois catetos e mostra a hipotenusa
 from math import hypot
-ca= float(input('Digite o cateto adjacente: '))
+ca = float(input('Digite o cateto adjacente: '))
 co = float(input('Digite o cateto oposto: '))
-print('A hipotenusa é',hypot(ca, co))
+print('A hipotenusa é', hypot(ca, co))
