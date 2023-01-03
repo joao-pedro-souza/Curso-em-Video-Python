a3ee32395736aaccf637f193e3b1e1f220f3837b
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-01-02 21:12:02 -0300

    comentei ex022

--- exercicios-curso-em-video/ex022.py
+++ exercicios-curso-em-video/ex022.py
@@ -1,9 +1,10 @@
+#Programa que lê um nome completo e mostra ele em letras maiúsculas, minúsculas e o primeiro nome
 nome = str(input('Digite seu nome completo: ')).strip()
 dividido = nome.split()
 espaços = nome.count(' ')
 print('Analisando seu nome...')
-print('Seu nome todo em maiúsculo é' ,nome.upper())
-print('Seu nome todo em minúsculo',nome.lower())
+print('Seu nome todo em maiúsculo é', nome.upper())
+print('Seu nome todo em minúsculo', nome.lower())
 print(f'O seu nome tem {len(nome)-espaços} letras')
 #print(f'O seu primeiro nome tem {len(dividido[0])} letras
 print(f'O seu primeiro nome tem {nome.find(" ")}')
