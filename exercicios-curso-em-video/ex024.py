90e4a8394bd1fb18a7219ad5676d0696ac5c6398
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-01-03 21:19:21 -0300

    comentei ex024

--- exercicios-curso-em-video/ex024.py
+++ exercicios-curso-em-video/ex024.py
@@ -1,3 +1,4 @@
+#Programa que lê o nome de uma cidade e mostra em letras maiúsculas, se a cidade começa com "Santo", mostra True na tela
 cidade = input('Em qual cidade você nasceu? ')
 cidade = cidade.upper()
 print(f'Você nasceu em Santo...? {"SANTO" in cidade}')
