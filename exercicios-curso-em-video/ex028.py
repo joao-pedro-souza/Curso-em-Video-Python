96b5003ff0547d58b51124b470b4221e901db6bb
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-01-04 20:03:16 -0300

    comentei ex028

--- exercicios-curso-em-video/ex028.py
+++ exercicios-curso-em-video/ex028.py
@@ -1,6 +1,7 @@
+#Programa que gera um número aleatório entre 1 e 5, e informa se o usuário acertou o número gerado
 from random import randint
 from time import sleep
-n = randint(1,5)
+n = randint(1, 5)
 print('==========JOGO DA ADIVINHAÇÃO==========')
 r = int(input('Chute um número de 1 à 5: '))
 print('CARREGANDO...')
@@ -9,5 +10,5 @@ if r == n:
     print(f'Parabéns, o número era {n}, você acertou')
 else:
     print(f'O número era {n}, você errou!')
-    
+
 
