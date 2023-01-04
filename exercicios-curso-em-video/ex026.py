14afb1864816c2672aa995eebe32780a4032400b
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-01-03 21:23:38 -0300

    comentei ex026

--- exercicios-curso-em-video/ex026.py
+++ exercicios-curso-em-video/ex026.py
@@ -1,4 +1,5 @@
+#Programa que lê uma frase e mostra quantas vezes a letra A aparece, e em qual posição apareceu pela primeira e pela última vez
 frase = input('Digite uma frase: ').strip()
 print(f'A letra A aparece {frase.upper().count("A")} vezes na frase.')
 print(f'A primeira letra A apareceu na posição {frase.upper().find("A")+1}')
-print(f'A letra A aparce pela última vez na posição {frase.upper().rfind("A")+1}')
+print(f'A letra A aparece pela última vez na posição {frase.upper().rfind("A")+1}')
