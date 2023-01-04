b88104bc66cc2d9cfa39b3b8dd821a4151c4ad99
João Pedro Souza <56794682+joao-pedro-souza@users.noreply.github.com>
2023-01-03 21:25:29 -0300

    comentei ex027

--- exercicios-curso-em-video/ex027.py
+++ exercicios-curso-em-video/ex027.py
@@ -1,3 +1,4 @@
+#Programa que lê um nome completo, separa e mostra o primeiro e último nome
 nome = input('Digite seu nome completo: ').strip()
 d = nome.split()
 print('Muito prazer em te conhecer!')
